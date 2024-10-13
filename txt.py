69363ff746cb0e97f22c71a00762067090b5912c
surajwali <surajwali11@gmail.com>
2024-10-13 09:04:07 +0000

    save files

---
+++ txt.py
@@ -0,0 +1,10 @@
+import os 
+folders = input("provide folder name").split()
+for folder in folders:
+    try:
+        files = os.listdir(folder)
+        print("listing the folder for the files",folder)
+        for file in files:
+          print(file)
+    except FileNotFoundError:
+        print("please provide the valid folder name ",folder)      
